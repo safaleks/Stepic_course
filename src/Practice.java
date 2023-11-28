0668dc6ef97a9e91f4f57c0cea53a635b29a8904
safaleks <safaleks@mail.ru>
2023-11-28 13:42:42 +0300

    Практическое задание на применение сканнера

---
+++ src/Practice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+    public class Practice {
+        public static void main(String[] args) {
+            //Напишите свой код здесь
+            Scanner scan = new Scanner(System.in);
+            String name = scan.nextLine();
+            System.out.print("Привет, " + name);
+        }
+    }
+
